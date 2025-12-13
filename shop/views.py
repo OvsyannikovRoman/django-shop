5b8ace7ea7ee251e4e6de0a8eb47dfb8653b9c4d
ОвсянниковРоман <romanovsyannikov512@gmail.com>
2025-12-13 17:06:05 +0200

    Completed Lab 2

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello WORLD!!!")
+
+from django.http import HttpResponse
+from .models import Category
+
+def index(request):
+    categories = Category.objects.all()
+    # Собираем все названия категорий в одну строку через запятую или пробел
+    response_text = '<br>'.join([str(category) for category in categories])
+    return HttpResponse(response_text)
